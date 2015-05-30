7c94e1068540b4a06f0608b923fc97bf86fb98fb
jouyang <oyjh1986@qq.com>
2015-05-30 09:06:23 +0800

    add signal_test

---
+++ signal_test.c
@@ -0,0 +1,52 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void sig_int(int signo);
+
+int main()
+{
+    sigset_t newmask, oldmask, waitmask;
+
+    if (signal(SIGINT, sig_int) == SIG_ERR)
+    {
+        perror("signal error\n");
+        exit(1);
+    }
+
+    sigemptyset(&waitmask);
+    sigaddset(&waitmask, SIGUSR1);
+    
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGINT);
+
+    if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
+    {
+        perror("sigprocmask error\n");
+        exit(1);
+    }
+
+    printf("in critical region\n");
+
+    if (sigsuspend(&waitmask) != -1)
+    {
+        perror("sigsuspend error");
+        exit(1);
+    }
+
+    printf("return from sigsuspend\n");
+
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+    {
+        perror("reset sigprocmask error\n");
+        exit(1);
+    }
+
+    return 0;
+}
+
+void sig_int(int signo)
+{
+    printf("sig init\n");
+}
+
